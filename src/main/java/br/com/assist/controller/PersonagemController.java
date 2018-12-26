9b650dcfddd98f185b93c6392d562bece3c99251
BrunoVieira319 <brunovieira319@hotmail.com>
2018-12-26 17:27:15 -0200

    adicionando magias

--- src/main/java/br/com/assist/controller/PersonagemController.java
+++ src/main/java/br/com/assist/controller/PersonagemController.java
@@ -7,29 +7,38 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.assist.domain.Personagem;
 import br.com.assist.service.PersonagemService;
 
 @RestController
-
+@RequestMapping(value = "/personagem")
 public class PersonagemController {
 
 	@Autowired
 	private PersonagemService service;
 
-	@GetMapping(value = "/personagem")
+	@GetMapping(value = "/")
 	public ResponseEntity<List<Personagem>> buscarTodosPersonagens() {
 		return new ResponseEntity<List<Personagem>>(service.listarTodos(), HttpStatus.OK);
 	}
-	
-	@PostMapping(value = "/personagem")
+
+	@PostMapping(value = "/")
 	public ResponseEntity<?> salvar(@RequestBody @Valid Personagem personagem) {
 		service.salvar(personagem);
+		return new ResponseEntity<>(HttpStatus.OK);
+	}
+
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<?> removerPersonagem(@PathVariable("id") Integer id) {
+		service.deletarPorId(id);
 		return new ResponseEntity<>(HttpStatus.OK);
 	}
 }
--- src/main/java/br/com/assist/domain/Magia.java
+++ src/main/java/br/com/assist/domain/Magia.java
@@ -1,17 +1,44 @@
 package br.com.assist.domain;
 
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
-public class Magia {
+public class Magia extends BaseDominio {
 
 	@NotNull
 	@NotEmpty
 	private String nome;
-	
+
 	@NotNull
+	private int nivel;
+
+	@NotNull
+	@Enumerated(EnumType.STRING)
+	private EscolaDeMagia escola;
+
+	@NotNull
+	private boolean ritual;
+
+	@NotNull
+	@NotEmpty
+	private String tempoDeConjuracao;
+
+	@NotNull
+	@NotEmpty
+	private String alcance;
+
+	@NotNull
+	@NotEmpty
+	private String componentes;
+
+	@NotNull
+	@NotEmpty
+	private String duracao;
+
+	@NotNull
+	@NotEmpty
 	private String descricao;
-	
-	@NotNull
-	private boolean preparada;
+
 }
